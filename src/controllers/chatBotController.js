b15a8c55cea1cf8ae7e4d03de598141b918d1755
destroyer342 <ranceofficial@gmail.com>
2022-03-13 06:39:26 +0800

    lowercase

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -103,7 +103,7 @@ function handleMessage(sender_psid, message) {
             "text": `your payload quickreply is : "${message.quick_reply.payload}"!`
         }
     } else {
-        message = message.toLowerCase()
+        message = message.text.toLowerCase()
         switch(message.text){
             case "names":    //user profile
                 request({
