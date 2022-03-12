6d1cf2906dba2890402c2252778fac2a726907c5
destroyer342 <ranceofficial@gmail.com>
2022-03-13 00:51:15 +0800

    rance

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -138,20 +138,20 @@ function handleMessage(sender_psid, message) {
             //         callSendAPIWithTemplate(sender_psid);
             //         break;
             //     default:
-            switch (message.text) {
-                case names:
-                    response1 = {
-                        "text": `this is the word ->: "${message.text}" !`
-                    }
-                    callSendAPI(sender_psid, response1);
-                    break;
+           // switch (message.text) {
+           //     case names:
+            //        response1 = {
+           //             "text": `this is the word ->: "${message.text}" !`
+           //         }
+             //       callSendAPI(sender_psid, response1);
+             //       break;
                     
-                default:
+          //      default:
                     response1 = {
                         "text": `You sent the message: "${message.text}" !`
                     }
-                    callSendAPI(sender_psid, response1);
-            }
+       //             callSendAPI(sender_psid, response1);
+           // }
 
 
 
