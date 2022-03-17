c9276cfc4fd810926d461dca747e82c67d683110
destroyer342 <ranceofficial@gmail.com>
2022-03-17 23:17:21 +0800

    rasda

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -285,7 +285,7 @@ let callQuickReply = (sender_psid) => {
               "payload":{
                 "template_type":"receipt",
                 "recipient_name":"Stephane Crozatier",
-                "order_number":"1234567a28222902",
+                "order_number":"1234567a228222902",
                 "currency":"USD",
                 "payment_method":"Visa 2345",        
                 "order_url":"http://originalcoastclothing.com/order?order_id=123456",
